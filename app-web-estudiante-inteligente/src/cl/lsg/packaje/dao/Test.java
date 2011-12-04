bbac35ec70b5b7505bf9e646d2d1fc2c54f29adb
Leonardo Castano <leonardo.castano@gmail.com>
2011-12-04 01:14:03 -0300

    se agrega agarra esto!!!

--- app-web-estudiante-inteligente/src/cl/lsg/packaje/dao/Test.java
+++ app-web-estudiante-inteligente/src/cl/lsg/packaje/dao/Test.java
@@ -13,7 +13,7 @@ public class Test implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 			System.out.println("IdTipoMensaje: " );
-			System.out.println("SMensaje tomá!!!!!: " );
+			System.out.println("SMensaje agarra esto!!!!!: " );
 		return null;
 	}
 
